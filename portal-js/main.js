b5eb9ddc004accca71887c16ab38b7f16ace1021
Swolebrain <thecodingteacher@gmail.com>
2016-07-09 16:50:36 -0400

    feat: Added modal application window and fixed navbar links

--- portal-js/main.js
+++ portal-js/main.js
@@ -8,3 +8,31 @@ function handleScroll(){
     $("nav").css("opacity", "0");
   }
 }
+
+$("#formsubmit").on("click", clickHandler);
+
+function clickHandler(){
+  $("#myModal .modal-footer span").html("Please Wait...");
+  $("#formsubmit").off("click");
+  var data = {
+    first_name: $("#fname").val(),
+    last_name: $("#lname").val(),
+    email: $("#email").val(),
+    phone1: $("#phone").val(),
+    program_id: "tech.fvi.edu"
+  }
+  $.ajax({
+    url: "http://fvi-grad.com:4004/submittechfviform",
+    method: 'POST',
+    success: function(resp, txt, xhr){
+      $("#myModal .modal-footer span").html(resp);
+      $("#formsubmit").on("click", clickHandler);
+      console.log(resp);
+      setTimeout(function(){$("#myModal .modal-footer span").html("");}, 1500);
+    },
+    data: data,
+    error: function(error, otherstuff){
+      $("#myModal .modal-footer span").html(error.statusText);
+    }
+  });
+}
